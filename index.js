86bf1d76ccd8aa993d3461ffd2635a367486a0ff
Albauralde <125832032+Albauralde@users.noreply.github.com>
2023-02-19 23:04:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+
+	function enviado(){alert('Su formulario ha sido enviado');}
+
+		
+
+		function contraseña(){
+					let confirmation = document.getElementById("confirmation").value
+					let clave = document.getElementById("Clave").value
+					
+					if(confirmation==clave)	
+					{submit()}
+					else{alert('Las contraseñas no coinciden');event.preventDefault()}}
+						
+
+	
+		function enviado(){alert('Su formulario ha sido enviado');}
+
+</script>
